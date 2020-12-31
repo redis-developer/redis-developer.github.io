57f3e1f02f3c14e8b0b180c58080a3102e48e86d
Tugdual Grall <tugdual@gmail.com>
2020-12-31 11:49:15 +0100

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,97 @@
+module.exports = {
+  title: 'Redis Developer',
+  tagline: 'The Home of Redis Developer',
+  url: 'https://redis-developer.github.io/redis-developer-site/',
+  baseUrl: ' /redis-developer-site/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'redis-developer', // Usually your GitHub org/user name.
+  projectName: 'redis-developer-site', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Redis Developer',
+      logo: {
+        alt: 'Redis Logo',
+        src: 'img/logos/redis.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Getting Started',
+          position: 'left',
+        },
+
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Develop',
+          items: [
+            {
+              label: 'Java',
+              to: 'docs/develop/java',
+            },
+
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/redis',
+            },
+            {
+              label: 'Discussion Forums',
+              href: 'https://forum.redislabs.com/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/redislabs',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              href: 'https://redislabs.com/blog/tech-blog/',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/redis-developer/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} RedisLabs Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Create',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        Create a new database using Cloud, Docker or from Sources
+      </>
+    ),
+  },
+  {
+    title: 'Explore',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        Insert, Update and Explore your database using RedisInsigh.
+      </>
+    ),
+  },
+  {
+    title: 'Code',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+        Develop your application with your favorite language.
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">The Home of Redis Developers</h1>
+          <p className="hero__subtitle">Made by Developers for Developers</p>
+
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+
+        <section>
+        <div className="container">
+            <h1>Resources by Languages</h1>
+            <p className="">Find documentation, sample code and tools to develop with your favorite language.</p>
+            <div className="row text--center">
+            <a class="col col--2" href={useBaseUrl('docs/develop/java')}>Java</a>            
+
+            <a class="col col--2" href={useBaseUrl('develop/python')}>Python</a>            
+
+            <a class="col col--2" href={useBaseUrl('develop/node')}>Node.js</a>            
+
+            <a class="col col--2" href={useBaseUrl('develop/golang')}>Golang</a>            
+
+
+            </div>
+
+
+        </div>
+        </section>
+
+        <section>
+        <div className="container">
+
+          <h1>Sample and Demos</h1>
+
+        </div>
+        </section>
+
+
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
