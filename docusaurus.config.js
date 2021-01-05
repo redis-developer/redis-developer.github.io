063956a26556b91d1909614b2c1f334ff41d8345
Tugdual Grall <tugdual@gmail.com>
2021-01-05 17:52:34 +0100

    fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,13 +81,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/redis-developer/redis-developer/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/redis-developer/redis-developer/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
