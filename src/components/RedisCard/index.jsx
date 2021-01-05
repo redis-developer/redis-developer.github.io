964171c59cfd3265ce7175324af8746211a05898
Tugdual Grall <tugdual@gmail.com>
2021-01-05 15:22:32 +0100

    Add Redis card for links

---
+++ src/components/RedisCard/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {MDXProvider} from '@mdx-js/react';
+import MDXComponents from '@theme/MDXComponents';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+import "./styles.css";
+
+const RoadmapItem = (props) => {
+    const [displayLong, setDisplayLong] = React.useState(false);
+
+    return <div className="ri-container">
+        <div className="ri-description-short">
+            <div className="ri-icon"><span className="fe fe-zap" /></div>
+            <div className="ri-detail">
+                <div className="ri-title"><a href={props.page}>{props.title}</a></div>
+                <div className="ri-description">
+                    {props.description}
+                    {React.Children.count(props.children) > 0 && <span className="ri-more fe fe-more-horizontal" onClick={() => setDisplayLong(!displayLong)}/>}
+
+                </div>
+            </div>
+        </div>
+        {displayLong && <div className="ri-description-long"><MDXProvider components={MDXComponents}>{props.children}</MDXProvider></div>}
+    </div>
+}
+
+export default RoadmapItem;
