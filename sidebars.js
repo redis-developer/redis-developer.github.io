d72c07ab60aa9b9bfbc8e78d20c68e4108689203
Tugdual Grall <tugdual@gmail.com>
2021-01-12 15:46:40 +0100

    add create page

--- sidebars.js
+++ sidebars.js
@@ -6,6 +6,7 @@ module.exports = {
       label: 'Home', // The label that should be displayed (string).
       href: '/' // The target URL (string).
     }],
+    Create: [ 'create/cloud/index-cloud', 'create/docker/index-docker', 'create/resoftware/index-software' ],
     Develop: ['develop/java/index-java', 'develop/node/index-node', 'develop/python/index-python', 'develop/golang/index-golang' ],
     'Tools & Guides': ['tools/index-tools','guides/index-guides'],
   },
