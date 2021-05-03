dc701f93b79eac7f69d0e51dfc80b34d715c5d63
Andrew Brookins <andrew.brookins@redislabs.com>
2021-05-03 14:54:52 -0700

    Add search based on redis-sitesearch

--- src/theme/LayoutHead/index.js
+++ src/theme/LayoutHead/index.js
@@ -59,6 +59,9 @@ export default function LayoutHead(props) {
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto+Mono:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+
+        <link href="/static/css/redis-sitesearch.css" rel="stylesheet"/>
+        <link href="/static/css/redis-sitesearch-overrides.css" rel="stylesheet"/>
       </Head>
 
       <SearchMetadatas tag={DEFAULT_SEARCH_TAG} locale={currentLocale} {...searchMetadatas} />
