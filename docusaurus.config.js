067e160b430d150c27d0d24d5739c1726c238339
Suze Shardlow <suzeshardlow@Suzes-MBP.lan>
2021-10-05 23:00:28 +0100

    Changes the config file.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ module.exports = {
     },
     announcementBar: {
       id: 'redisconf20201cfp', // Any value that will identify this message.
-      content: '<p class="text">We\'re supporting Hacktoberfest!</p> <a href="/hacktoberfest" class="btn">Join Us!</a>',
+      content: '<p class="text">We\'re taking part in Hacktoberfest!</p> <a href="/hacktoberfest" class="btn">Join Us!</a>',
     //  content: '<p class="text"></p> <a href="https://www.youtube.com/c/Redisinc/playlists?view=50&sort=dd&shelf_id=1" target="_blank" rel="noopener" class="btn"></a>',
       backgroundColor: '#fff', // Defaults to `#fff`.
       textColor: '#000', // Defaults to `#000`.
