533a447fbe602d844ce9816c9dbc16ecedef44eb
ajeetraina <ajeetraina@gmail.com>
2022-02-24 12:42:07 +0530

    Added New Tutorial for RedisInsight v2 for RediSearch

--- sidebars.js
+++ sidebars.js
@@ -277,6 +277,7 @@ module.exports = {
            'explore/redisinsightv2/getting-started/index-gettingstarted',
            'explore/redisinsightv2/browser/index-browser',
            'explore/redisinsightv2/profiler/index-profiler',
+           'explore/redisinsightv2/redisearch/index-redisearch',
            ]
          },
         {
