adc949199c83266f98e1b1e484266de534574d35
gh-actions <actions@gihub.com>
2023-07-05 16:12:45 +0000

    Deploy website - based on 6b8788f5d5d513f4196dfd7366c25f508274db4c

--- scripts/custom-page-script.js
+++
@@ -1,7 +0,0 @@
-export function onRouteUpdate({ location, previousLocation }) {
-    if (location && location.pathname.match("/howtos/redisgraph/")) {
-
-        window.location.replace("/howtos/vector-db");
-    }
-    return undefined;
-}
